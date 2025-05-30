1fc2bab719398110cff95702090244d1ba5280b8
Vignesh Ramesh <vigneshramesh448@gmail.com>
2025-05-30 23:30:45 +0530

    Update and rename README.md to insetAtBeg.c
    insertAtBeg refers to inserting a new node at the beginning (head) of a linked list.

---
+++ insetAtBeg.c
@@ -0,0 +1,38 @@
+# Data-Structure
+Program Code
+
+#include <stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node*next;
+};
+struct Node*head=NULL;
+void insertAtBeg(int data)
+{
+    struct Node*newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=data;
+    newNode->next=head;
+    head=newNode;
+}
+void display()
+{
+    struct Node*temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+}
+int main()
+{
+insertAtBeg(5);
+insertAtBeg(6);
+insertAtBeg(7);
+insertAtBeg(8);
+insertAtBeg(9);
+display();
+    return 0;
+}
